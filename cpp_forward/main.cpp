1ac88e12a1febddef3166a9f69d2854352701161
bigballon <605718199@qq.com>
2018-07-04 21:09:45 +0800

    add CPU forward

--- cpp_forward/main.cpp
+++ cpp_forward/main.cpp
@@ -9,11 +9,20 @@
 #include "caffe2/core/common.h"
 #include "caffe2/utils/proto_utils.h"
 #include "caffe2/core/workspace.h"
-#include "caffe2/core/context_gpu.h"
 #include "caffe2/core/tensor.h"
 #include "caffe2/core/init.h"
 
-// headers for opencv [P.S]
+
+// feel free to define USE_GPU if you want to use gpu
+
+// #define USE_GPU
+
+
+#ifdef USE_GPU
+#include "caffe2/core/context_gpu.h"
+#endif
+
+// headers for opencv 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -28,19 +37,10 @@ CAFFE2_DEFINE_string(predict_net, "./predict_net.pb",
                      "The given path to the predict protobuffer.");
 CAFFE2_DEFINE_string(file, "./image_file.jpg", "The image file.");
 
-// define functons
-void forwardTest();
-void loadImage(std::string file_name, float* imgBinary);
 
-// main function
-int main(int argc, char** argv) {
-    caffe2::GlobalInit(&argc, &argv);
-    forwardTest();
-    google::protobuf::ShutdownProtobufLibrary();
-    return 0;
-}
+namespace caffe2{
 
-void loadImage(std::string file_name, float* imgBinary){
+void loadImage(std::string file_name, float* imgArray){
 
     auto image = cv::imread(file_name);    // CV_8UC3
     std::cout << "== image size: " << image.size()
@@ -63,49 +63,66 @@ void loadImage(std::string file_name, float* imgBinary){
         data.insert(data.end(), (float *)c.datastart, (float *)c.dataend);
     }
     
-    // do normalization & copy to imgBinary
+    // do normalization & copy to imgArray
     int dim = 0;
     float image_mean[3] = {113.865, 122.95, 125.307};
     float image_std[3] = {66.7048, 62.0887, 62.9932};
         
     for(auto i = 0; i < data.size();++i){
         if(i > 0 && i % (32*32) == 0) dim++;
-        imgBinary[i] = (data[i] - image_std[dim]) / image_mean[dim];
-        // std::cout << imgBinary[i] << std::endl;
+        imgArray[i] = (data[i] - image_std[dim]) / image_mean[dim];
+        // std::cout << imgArray[i] << std::endl;
     }
 }
 
-void forwardTest(){
+void run(){
 
     // define a caffe2 Workspace
-    caffe2::Workspace workSpace;
+    Workspace workSpace;
 
     // define initNet and predictNet
-    caffe2::NetDef initNet, predictNet;
+    NetDef initNet, predictNet;
 
     // read protobuf
     CAFFE_ENFORCE(ReadProtoFromFile(FLAGS_init_net, &initNet));
     CAFFE_ENFORCE(ReadProtoFromFile(FLAGS_predict_net, &predictNet));
 
-    // set device type, I just test GPU version
-    predictNet.mutable_device_option()->set_device_type(caffe2::CUDA);
-    initNet.mutable_device_option()->set_device_type(caffe2::CUDA);
+    // set device type
+#ifdef USE_GPU
+    predictNet.mutable_device_option()->set_device_type(CUDA);
+    initNet.mutable_device_option()->set_device_type(CUDA);
+#else
+    predictNet.mutable_device_option()->set_device_type(CPU);
+    initNet.mutable_device_option()->set_device_type(CPU);
+
+    for(int i = 0; i < predictNet.op_size(); ++i){
+        predictNet.mutable_op(i)->mutable_device_option()->set_device_type(CPU);
+    }
+    for(int i = 0; i < initNet.op_size(); ++i){
+        initNet.mutable_op(i)->mutable_device_option()->set_device_type(CPU);
+    }
+#endif
 
     // load network
     CAFFE_ENFORCE(workSpace.RunNetOnce(initNet));
     CAFFE_ENFORCE(workSpace.CreateNet(predictNet));
 
     // load image from file, then convert it to float array.
-    float imgBinary[3 * 32 * 32];
-    loadImage(FLAGS_file, imgBinary);
+    float imgArray[3 * 32 * 32];
+    loadImage(FLAGS_file, imgArray);
 
     // define a Tensor which is used to stone input data
-    caffe2::TensorCPU input;
-    input.Resize(std::vector<caffe2::TIndex>({1, 3, 32, 32}));
-    input.ShareExternalPointer(imgBinary);
+    TensorCPU input;
+    input.Resize(std::vector<TIndex>({1, 3, 32, 32}));
+    input.ShareExternalPointer(imgArray);
 
     // get "data" blob
-    auto data = workSpace.GetBlob("data")->GetMutable<caffe2::TensorCUDA>();
+#ifdef USE_GPU
+    auto data = workSpace.GetBlob("data")->GetMutable<TensorCUDA>();
+#else
+    auto data = workSpace.GetBlob("data")->GetMutable<TensorCPU>();
+#endif
+
     // copy from input data
     data->CopyFrom(input);
 
@@ -116,8 +133,12 @@ void forwardTest(){
     std::vector<std::string> labelName = {"airplane","automobile","bird","cat","deer",
         "dog","frog","horse","ship","truck"};
 
-    auto softmax = caffe2::TensorCPU(workSpace.GetBlob("softmax")->Get<caffe2::TensorCUDA>());
-    
+#ifdef USE_GPU
+    auto softmax = TensorCPU(workSpace.GetBlob("softmax")->Get<TensorCUDA>());
+#else
+    auto softmax = workSpace.GetBlob("softmax")->Get<TensorCPU>();
+#endif
+
     std::vector<float> probs(softmax.data<float>(),
         softmax.data<float>() + softmax.size());
     
@@ -126,4 +147,14 @@ void forwardTest(){
     std::cout << "== predicted label: " << labelName[index] 
               << " ==\n== with probability: " << (*max * 100)
               << "% ==" << std::endl;
+}
+
+}    // namespace caffe2
+
+// main function
+int main(int argc, char** argv) {
+    caffe2::GlobalInit(&argc, &argv);
+    caffe2::run();
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
 }
